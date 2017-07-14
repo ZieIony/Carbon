cf1bf25f80e578b8c137a20cda919b1dfbd56414
Zielony <niewartotupisac@gmail.com>
2017-07-15 00:14:56 +0200

    updated FlowLayout & Chips sample, added more images for Wiki

--- samples/src/main/java/tk/zielony/carbonsamples/widget/FlowLayoutActivity.java
+++ samples/src/main/java/tk/zielony/carbonsamples/widget/FlowLayoutActivity.java
@@ -2,23 +2,20 @@ package tk.zielony.carbonsamples.widget;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
 
 import com.annimon.stream.Stream;
-import com.squareup.picasso.Picasso;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import carbon.widget.Chip;
 import carbon.widget.FlowLayout;
 import tk.zielony.carbonsamples.R;
 import tk.zielony.carbonsamples.Samples;
 import tk.zielony.carbonsamples.SamplesActivity;
+import tk.zielony.randomdata.DataContext;
+import tk.zielony.randomdata.person.DrawableAvatarGenerator;
+import tk.zielony.randomdata.person.Gender;
+import tk.zielony.randomdata.person.StringFirstNameGenerator;
 
 public class FlowLayoutActivity extends SamplesActivity {
-    private static List<String> fruits = new ArrayList<>(Arrays.asList("Strawberry", "Apple", "Orange", "Lemon", "Beer", "Lime", "Watermelon", "Blueberry", "Plum"));
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,26 +24,19 @@ public class FlowLayoutActivity extends SamplesActivity {
 
         Samples.initToolbar(this, getString(R.string.flowLayoutActivity_title));
 
+        DrawableAvatarGenerator avatarGenerator = new DrawableAvatarGenerator(this);
+        StringFirstNameGenerator nameGenerator = new StringFirstNameGenerator(Gender.Both, false, false);
+
         FlowLayout layout = findViewById(R.id.flowLayout);
         Stream.of(layout.getViews()).filter(v -> v instanceof Chip).forEach(v -> {
             final Chip chip = (Chip) v;
-            chip.setText(fruits.get((int) (Math.random() * fruits.size())));
-            if (Math.random() > 0.5) {
-                chip.setIconVisible(true);
-                String image = "http://lorempixel.com/100/100/people/#" + System.currentTimeMillis();
-                Picasso.with(this).load(image).into((ImageView) chip.getIconView());
-            } else {
-                chip.setIconVisible(false);
-            }
-            if (Math.random() > 0.5) {
-                chip.setRemovable(true);
-                chip.setOnRemoveListener(() -> {
-                    //       chip.setOutAnimator(AnimUtils.Style.Fade);
-                    chip.setVisibility(View.GONE);
-                });
-            } else {
-                chip.setRemovable(false);
-            }
+            DataContext dataContext = new DataContext();
+            chip.setText(nameGenerator.next(dataContext));
+            chip.setIcon(avatarGenerator.next(dataContext));
+            chip.setRemovable(true);
+            chip.setOnRemoveListener(() -> {
+                chip.setVisibility(View.GONE);
+            });
         });
     }
 }
